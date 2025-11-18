517d5fc09106491fb4c9a45253c3fa8d09ff29c3
z5525093 <z5525093@ad.unsw.edu.au>
2025-11-18 15:06:49 +1100

    merge alg_main to main.asm for search path algorithm

--- main.asm
+++ main.asm
@@ -39,8 +39,10 @@
 ; ------------------------------------------------------------------------------
 ; Constants (update once assumptions are refined)
 ; ------------------------------------------------------------------------------
-.equ MAP_SIZE           = 15                 ; maximum grid 15x15
-.equ MAP_CELLS          = (MAP_SIZE*MAP_SIZE)
+.equ size3 = 15
+.equ l3 = size3 * size3
+.equ MAP_SIZE           = 7                 ; maximum grid 15x15
+.equ MAP_CELLS          = 49
 .equ MAX_OBS_POINTS     = 16           ; supports multiple coverage groups
 .equ OBS_POINT_STRIDE   = 3            ; x, y, z (height)
 .equ PATH_BUF_BYTES     = MAX_OBS_POINTS * OBS_POINT_STRIDE
@@ -251,6 +253,226 @@ busy_loop:
 	out DDRF, temp7
 .endmacro
 
+;int* array_i_j(array, int size, Rd i, Rd j)
+; Description: array[j][i]
+;Rd != r16/r17, r16: temp, r17: temp
+.macro array_i_j
+	push yh
+	push yl
+	push r16
+	in r16, SREG
+	push r16
+	push r17
+
+	ldi yh, high(@0)
+	ldi yl, low(@0)
+	mov r16, @3
+	ldi r17, @1
+	mul r16, r17
+	mov r16, r0
+	mov r17, @2
+	neg r17
+	sub r16, r17
+	;dec r16
+	add yl, r16
+	ldi r16, 0
+	adc yh, r16
+	mov xl, yl
+	mov xh, yh
+
+	pop r17
+	pop r16
+	out SREG, r16
+	pop r16
+	pop yl
+	pop yh
+.endmacro
+
+;int count_diff(c_vis, p_vis, int length, Rd)
+;Description: Count the difference between two vis arrays
+;r16: c_vis[j][i] r17: p_vis[j][i], r18: counter, Rd != r16/r17
+.macro count_diff
+	push yh
+	push yl
+	push xh
+	push xl
+	push r16
+	in r16, SREG
+	push r16
+	push r17
+	push r18
+
+	ldi yh, high(@0)
+	ldi yl, low(@0)
+	ldi xh, high(@1)
+	ldi xl, low(@1)
+	clr r18
+	clr @3
+count_diff_cond:
+	cpi r18, @2
+	brsh count_diff_end
+	ld r16, y+
+	ld r17, x+
+	cp r16, r17
+	breq count_diff_same
+	inc @3
+count_diff_same:
+	inc r18
+	rjmp count_diff_cond
+count_diff_end:
+	pop r18
+	pop r17
+	pop r16
+	out SREG, r16
+	pop	r16
+	pop xl
+	pop xh
+	pop yl
+	pop yh
+.endmacro
+
+; void greedy_search()
+; Description: Greedy search
+; r18 = org_x, r19 = org_y, r20 = cur_x, r21 = cur_y, r22 = visability, r2 = diff, r3 = max_diff, r4 = max_x, r5 = max_y, r6 = counter, r23 = y, r24 = x, r25 = temp
+.macro greedy_search
+	push yh
+	push yl
+	push xh
+	push xl
+
+	push r2
+	push r3
+	push r4
+	push r5
+	push r6
+	push r18
+	push r19
+	push r20
+	push r21
+	push r22
+	push r23
+	push r24
+	push r25
+	in r25, SREG
+	push r25
+
+	; counter = 0
+	clr r6
+greedy_search_start:
+	; max_diff = 0
+	clr r2
+	clr r3
+	clr r4
+	clr r5
+	clr r23
+	clr r24
+y_for_loop:
+	; for y in range(len(mountain))
+	clr r24
+	cpi r23, size3
+	in r25, SREG
+	sbrc r25, 1
+	jmp y_for_loop_end
+	x_for_loop:
+		; for x in range(len(mountain))
+		cpi r24, size3
+		in r25, SREG
+		sbrc r25, 1
+		jmp x_for_loop_end
+		if_pre_v_y_x_eq_0:
+			; if pre_v[y][x] == 0
+			array_i_j p_vis, size3, r24, r23
+			ld r25, x
+			cpi r25, 0
+			in r25, SREG
+			sbrs r25, 1
+			jmp if_pre_v_y_x_eq_0_end
+		
+			; cur_v = [[pre_v[j][i] for i in range(len(mountain))] for j in range(len(mountain))]
+			load_array_from_data c_vis, p_vis, l3
+			; check = [[0 for _ in range(len(mountain))] for _ in range(len(mountain))]
+			load_array_from_program zeros, check, l3
+			; dfs(x, y, x, y, visability, p_index)
+			mov r18, r24
+			mov r19, r23
+			mov r20, r24
+			mov r21, r23
+			lds r22, Visibility
+			call dfs
+			; diff = count_diff(cur_v, pre_v)
+			count_diff c_vis, p_vis, l3, r2
+			if_diff_g_max_diff:
+				; if diff > max_diff
+				cp r3, r2
+				in r25, SREG
+				sbrs r25, 0
+				jmp if_diff_g_max_diff_end
+				; max_diff = int(diff)
+				mov r3, r2
+				; max_point = (x, y)
+				mov r4, r24
+				mov r5, r23
+				; max_map = [[cur_v[j][i] for i in range(len(mountain))] for j in range(len(mountain))]
+				load_array_from_data max_map, c_vis, l3
+			if_diff_g_max_diff_end:
+		if_pre_v_y_x_eq_0_end:
+		; x++
+		inc r24
+		rjmp x_for_loop
+	x_for_loop_end:
+	; y++
+	inc r23
+	rjmp y_for_loop
+y_for_loop_end:
+	; if max_diff == 0: break
+	ldi r25, 0
+	cp r3, r25
+	breq greedy_search_end
+	; point.append(max_point)
+	ldi xh, high(obs_x)
+	ldi xl, low(obs_x)
+	add xl, r6
+	ldi r25, 0
+	adc xh, r25
+	st x, r4
+
+	ldi xh, high(obs_y)
+	ldi xl, low(obs_y)
+	add xl, r6
+	ldi r25, 0
+	adc xh, r25
+	st x, r5
+	; pre_v = [[max_map[j][i] for i in range(len(mountain))] for j in range(len(mountain))]
+	load_array_from_data p_vis, max_map, l3
+	;counter++
+	inc r6
+	jmp greedy_search_start
+greedy_search_end:
+	ldi xh, high(obs_len)
+	ldi xl, low(obs_len)
+	st x, r6
+
+	pop r25
+	out SREG, r25
+	pop r25
+	pop r24
+	pop r23
+	pop r22
+	pop r21
+	pop r20
+	pop r19
+	pop r18
+	pop r6
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop xl
+	pop xh
+	pop yl
+	pop yh
+.endmacro
+
 ; ------------------------------------------------------------------------------
 ; Data memory layout
 ; ------------------------------------------------------------------------------
@@ -280,8 +502,16 @@ YEditCnt:              .byte 1
 VEditVal:              .byte 1
 VEditCnt:              .byte 1
 
-	; ----- Part 3: terrain + path data -----
-MountainMatrix:        .byte MAP_CELLS
+	; ----- Part 3: terrain + path data -----map:	.byte l3
+map:	.byte l3
+c_vis:	.byte l3
+p_vis:	.byte l3
+check:	.byte l3
+max_map: .byte l3
+obs_x:	.byte l3
+obs_y:	.byte l3
+obs_z:	.byte l3
+obs_len: .byte 1
 CoverageMask:          .byte MAP_CELLS  ; 0 = unseen, 1 = covered
 ObservationPath:       .byte PATH_BUF_BYTES
 PathLength:            .byte 1          ; number of stored observation points
@@ -311,16 +541,16 @@ PointRenderBuf:        .byte 8          ; e.g., "3,3,6/"
 ; Interrupt vector table (add ISRs as needed)
 ; ------------------------------------------------------------------------------
 .org 0x0000
-	jmp RESET
+    jmp RESET
 .org INT0addr
-	jmp INT0_ISR            ; PB0 if used as interrupt
+    jmp INT0_ISR
 .org INT1addr
-	jmp INT1_ISR            ; PB1 if used as interrupt
-; Ensure ascending vector order to avoid .cseg overlap.
-.org OVF2addr
-	jmp TIMER2_OVF_ISR      ; optional LED blink or keypad debounce
-.org OVF0addr
-	jmp TIMER0_OVF_ISR      ; used for 10 ms tick
+    jmp INT1_ISR
+.org OVF2addr            ; 0x002E
+    jmp TIMER2_OVF_ISR
+.org OVF0addr            ; 0x0030
+    jmp TIMER0_OVF_ISR
+
 
 ; ------------------------------------------------------------------------------
 ; ------------------------------------------------------------------------------
@@ -356,12 +586,6 @@ MainLoop:
 ; Part 1: Platform services (initialisation, LCD, keypad, timing)
 ; ==============================================================================
 
-InitStack:
-	ldi workA, high(RAMEND)
-	out SPH, workA
-	ldi workA, low(RAMEND)
-	out SPL, workA
-	ret
 
 InitRegisters:
 	clr temp0
@@ -859,6 +1083,245 @@ HandlePB0PathGen:
 	; TODO: respond to PB0 press to trigger Part 3
 	ret
 
+GenerateSearchPath:
+	PUSH_CTX
+	push workA
+	push workB
+	push workC
+	push workD
+	push workE
+	push workF
+	push workG
+	push YH
+	push YL
+	push ZH
+	push ZL
+	push XH
+	push XL
+
+	; Initialise working maps and observation buffers
+	load_array_from_program m, map, l3
+	load_array_from_program zeros, p_vis, l3
+	load_array_from_program zeros, c_vis, l3
+	load_array_from_program zeros, check, l3
+	load_array_from_program zeros, max_map, l3
+	load_array_from_program zeros, obs_x, l3
+	load_array_from_program zeros, obs_y, l3
+
+	; Run greedy search to populate obs_x/obs_y and obs_len
+	greedy_search
+
+	; Prepare pointers for copying into ObservationPath
+	ldi YL, low(obs_x)
+	ldi YH, high(obs_x)
+	ldi XL, low(obs_y)
+	ldi XH, high(obs_y)
+	ldi ZL, low(ObservationPath)
+	ldi ZH, high(ObservationPath)
+
+	lds workC, obs_len
+	cpi workC, MAX_OBS_POINTS
+	brlo gsp_len_ok
+	ldi workC, MAX_OBS_POINTS
+gsp_len_ok:
+	clr workB
+	tst workC
+	breq gsp_copy_done
+
+gsp_copy_loop:
+	; load next observation (x from obs_x, y from obs_y)
+	ld workD, Y+
+	ld workE, X+
+	st Z+, workD
+	st Z+, workE
+	; preserve obs_y pointer across array_i_j call
+	mov temp12, XL
+	mov temp13, XH
+	; fetch terrain height for this coordinate
+	mov temp0, workD
+	mov temp1, workE
+	array_i_j map, size3, temp0, temp1
+	ld workF, x
+	st Z+, workF
+	; restore obs_y pointer
+	mov XL, temp12
+	mov XH, temp13
+	inc workB
+	cp workB, workC
+	brlo gsp_copy_loop
+
+gsp_copy_done:
+	sts PathLength, workB
+	clr workA
+	sts PathIndex, workA
+	sts ScrollHead, workA
+
+	pop XL
+	pop XH
+	pop ZL
+	pop ZH
+	pop YL
+	pop YH
+	pop workG
+	pop workF
+	pop workE
+	pop workD
+	pop workC
+	pop workB
+	pop workA
+	POP_CTX
+	ret
+
+; void dfs(org_x, org_y, cur_x, cur_y, visability)
+; Description: dfs
+; r18 = org_x, r19 = org_y, r20 = cur_x, r21 = cur_y, r22 = visability, r23, r24 = temp
+dfs:
+	push yh
+	push yl
+	push xh
+	push xl
+	push r23
+	in r23, SREG
+	push r23
+	push r24
+	in yl, SPL
+	in yh, SPH
+	sbiw y, 5
+	out SPH, yh
+	out SPL, yl
+	std y+1, r18
+	std y+2, r19
+	std y+3, r20
+	std y+4, r21
+	std y+5, r22
+	
+	ldd r22, y+5
+	ldd r21, y+4
+	ldd r20, y+3
+	ldd r19, y+2
+	ldd r18, y+1
+
+	; if check[cur_y][cur_x] == 1:
+	array_i_j check, size3, r20, r21
+	ld r23, x
+	cpi r23, 1
+	in r24, SREG
+	sbrc r24, 1
+	jmp dfs_end
+
+	; if (org_x - cur_x)**2 + (org_y - cur_y)**2 + (mountain[org_y][org_x] - mountain[cur_y][cur_x])**2 <= v**2:
+	array_i_j map, size3, r18, r19
+	ld r23, x
+	array_i_j map, size3, r20, r21
+	ld r24, x
+	sub r23, r24
+	brpl positive_dz
+	neg r23
+	positive_dz:
+
+	sub r18, r20
+	brpl positive_dx
+	neg r18
+	positive_dx:
+
+	sub r19, r21
+	brpl positive_dy
+	neg r19
+	positive_dy:
+
+	mul r18, r18
+	mov r18, r0
+
+	mul r19, r19
+	mov r19, r0
+
+	mul r23, r23
+	mov r23, r0
+
+	mul r22, r22
+	mov r22, r0
+
+	add r18, r19
+	in r24, SREG
+	sbrc r24, 3
+	jmp dfs_end
+
+	add r18, r23
+	in r24, SREG
+	sbrc r24, 3
+	jmp dfs_end
+
+	cp r22, r18
+	in r24, SREG
+	sbrc r24, 0
+	jmp dfs_end
+
+	; c_vis[cur_y][cur_x] = 1
+	array_i_j c_vis, size3, r20, r21
+	ldi r23, 1
+	st x, r23
+
+	; check[cur_y][cur_x] = 1
+	array_i_j check, size3, r20, r21
+	ldi r23, 1
+	st x, r23
+
+left_if:
+	ldd r22, y+5
+	ldd r21, y+4
+	ldd r20, y+3
+	ldd r19, y+2
+	ldd r18, y+1
+	; if cur_x - 1 >= 0:
+	subi r20, 1
+	brmi right_if
+	rcall dfs
+right_if:
+	ldd r22, y+5
+	ldd r21, y+4
+	ldd r20, y+3
+	ldd r19, y+2
+	ldd r18, y+1
+	; if cur_x + 1 < len(mountain):
+	inc r20
+	cpi r20, size3
+	brsh down_if
+	rcall dfs
+down_if:
+	ldd r22, y+5
+	ldd r21, y+4
+	ldd r20, y+3
+	ldd r19, y+2
+	ldd r18, y+1
+	; if cur_y - 1 >= 0:
+	subi r21, 1
+	brmi up_if
+	rcall dfs
+up_if:
+	ldd r22, y+5
+	ldd r21, y+4
+	ldd r20, y+3
+	ldd r19, y+2
+	ldd r18, y+1
+	; if cur_y + 1 < len(mountain):
+	inc r21
+	cpi r21, size3
+	brsh dfs_end
+	rcall dfs
+dfs_end:
+	adiw y, 5
+	out SPH, yh
+	out SPL, yl
+	pop r24
+	pop r23
+	out SREG, r23
+	pop r23
+	pop xl
+	pop xh
+	pop yl
+	pop yh
+	ret
+
 BeginScrollPreview:
 	; TODO: executed after PB0 path generation to enter STATE_SCROLL_PATH
 	ret
@@ -923,6 +1386,7 @@ HandleConfigState:
 hc_do_s4:
     rcall ResetCoverageMap
     rcall GenerateSearchPath
+	; if PathLength > 0, set S4 flag and move to scroll
     lds workA, PathLength
     cpi workA, 0
     breq hc_check_pb0
@@ -1529,7 +1993,7 @@ rcm_loop:
 	pop YL
 	ret
 
-GenerateSearchPath:
+GenerateSearchPathStub:
 	; Build a trivial non-empty path: (X,Y,0) and (X,Y,Vis)
 	push YL
 	push YH
@@ -1787,7 +2251,7 @@ m: .db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, \
 		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, \
 		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-v: .db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, \
+zeros: .db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, \
 		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, \
 		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, \
 		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, \
